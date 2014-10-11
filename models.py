2b2c6f07787754d2e403245e42da187520f55d02
rockwotj <rockwotj@rose-hulman.edu>
2014-10-10 23:25:26 -0400

    added the baseline of the upload script and some other misc placeholders

---
+++ models.py
@@ -0,0 +1,12 @@
+'''
+
+This module contains all the datastore models for the website's datastore.
+
+Created on Oct 10, 2014
+@author: rockwotj
+'''
+from google.appengine.ext import ndb
+
+
+class File(ndb.Model):
+    pass
