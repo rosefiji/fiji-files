23414d0381d4a312ed49ddb67165fffd102b9722
rockwotj <rockwotj@rose-hulman.edu>
2014-11-11 23:28:53 -0500

    Changed the models

--- models.py
+++ models.py
@@ -13,9 +13,11 @@ DEPARTMENTS = ['AB', 'BE', 'BIO', 'CE', 'CHE', 'CHEM', 'CSSE', 'ECE', 'EM', 'EMG
 
 class File(ndb.Model):
     department = ndb.StringProperty(required=True, choices=DEPARTMENTS)
-    classNumber = ndb.IntegerProperty(required=True)
+    classNumber = ndb.IntegerProperty(required=True, choices=range(101, 600))
     professor = ndb.StringProperty(required=True)
     fileType = ndb.StringProperty(required=True, choices=FILE_TYPES)
-    TermCode = ndb.IntegerProperty(required=True)  # 201510 is FallQtr for the 2014-2015 school year
+    termcode = ndb.IntegerProperty(required=True)  # 201510 is FallQtr for the 2014-2015 school year
+    FileUrl = ndb.StringProperty(required=True)  # This is the link to the pdf file
+    Comments = ndb.TextProperty()
 
 
